e5446c780bf0c3743b5a6589497f77e640949489
Basti Buck <mail@bastibuck.de>
2023-01-20 16:50:55 +0100

    make name mandatory

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const NewGroupSchema = z.object({
-  name: z.string(),
+  name: z.string().min(5, "Name must contain at least 5 characters"),
   description: z.string().optional(),
 });
 
