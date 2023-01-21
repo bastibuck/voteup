8779fbc3bd1299a1bf9d90e15d7d93ef498422ae
Basti Buck <mail@bastibuck.de>
2023-01-21 15:54:28 +0100

    remove unused themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: [
-      // "bumblebee",
-      // "emerald",
-      "corporate",
-      // "garden",
-      // "wireframe",
-      // "dracula",
-      // "cmyk",
-      // "autumn",
-      // "lemonade",
-      "winter",
-    ],
+    themes: ["corporate"],
   },
 };
