bef325c3e785d25e9af73d4446ddcfd9b2a63f34
Basti Buck <mail@bastibuck.de>
2023-11-22 14:38:42 +0100

    use coffee theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,14 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["corporate"],
+    // themes: ["corporate"],
+    // themes: ["dark"],
+    // themes: ["emerald"],
+    // themes: ["forest"],
+    // themes: ["lofi"],
+    // themes: ["synthwave"],
+    // themes: ["halloween"],
+    // themes: ["wireframe"],
+    themes: ["coffee"],
   },
 };
